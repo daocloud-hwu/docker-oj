1564d6de4727c29ffc68a6ac5a933bddb8660cf7
JinweiClarkChao <clarkzjw@gmail.com>
2015-05-08 09:33:06 +0800

    start

--- root/start.sh
+++ root/start.sh
@@ -2,5 +2,4 @@
 export GOPATH=/home/acm/go
 export PATH=$GOPATH/bin:$PATH
 mongod --fork --logpath /home/acm/Data/mongo.log -port 8090 --dbpath /home/acm/Data
-/home/acm/go/src/GoOnlineJudge/RunServer &
-restweb run /home/acm/go/src/GoOnlineJudge &
+restweb run /home/acm/go/src/GoOnlineJudge
