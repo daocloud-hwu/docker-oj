592822895e3fadee814e8a43a9f36f862d91f46a
JinweiClarkChao <clarkzjw@gmail.com>
2015-05-08 10:21:37 +0800

    fix path

--- root/start.sh
+++ root/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export GOPATH=/home/acm/go
-export PATH=$GOPATH/bin:$PATH
+export GOROOT=/goroot
+export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 mongod --fork --logpath /home/acm/Data/mongo.log -port 8090 --dbpath /home/acm/Data
 cd $GOPATH/src
 restweb run GoOnlineJudge
